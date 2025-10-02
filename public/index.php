7bdc528d14b416a3fe5f9643adbb51d7608b52a6
Yana <yanachistyakova458@gmail.com>
2025-10-02 16:07:01 +0000

    Index.php

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php
+// Definimos el título de la página
+$titulo_pagina = "Inicio - Enciclopedia de Animales";
+
+// Incluimos cabecera
+include '../includes/header.php';
+
+// Incluimos el footer
+include '../includes/header.php';
+
+// Importamos los datos
+require_once '../data/datos.php';
+?>
